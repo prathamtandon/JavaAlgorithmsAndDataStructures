c1eaaaead61d101f065230de790e0c2a5e65d947
Pratham Tandon <prathamt@outlook.com>
2016-08-12 19:54:36 -0700

    Add Graph class as adjacency list

---
+++ src/main/java/Graphs/Graph.java
@@ -0,0 +1,40 @@
+package Graphs;
+
+import Collections.Bag;
+
+/**
+ * Created by prathamt on 8/12/16.
+ */
+public class Graph {
+
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[])new Bag[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Bag<Integer>();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+}
