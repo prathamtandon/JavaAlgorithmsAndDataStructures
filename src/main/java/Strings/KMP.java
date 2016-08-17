643218a4dca4c5fec3446b1c0e67ad4491aa9f5f
Pratham Tandon <prathamt@outlook.com>
2016-08-17 15:59:31 -0700

    KMP substring matching

---
+++ src/main/java/Strings/KMP.java
@@ -0,0 +1,48 @@
+package Strings;
+
+/**
+ * Created by prathamt on 8/17/16.
+ */
+public class KMP {
+
+    private String pat;
+    private int[][] dfa;
+
+    public KMP(String pat) {
+        // Build DFA from pattern
+        this.pat = pat;
+        int M = pat.length();
+        int R = 256;
+        dfa = new int[R][M];
+        dfa[pat.charAt(0)][0] = 1;
+        for(int X = 0, j = 1; j < M; j++) {
+            // Compute dfa[][j] ie all possible transitions from state 'j'
+            for(int c = 0; c < R; c++)
+                dfa[c][j] = dfa[c][X];          //  Copy mismatch cases
+            dfa[pat.charAt(j)][j] = j+1;        //  Set match case
+            X = dfa[pat.charAt(j)][X];          //  Update restart state
+        }
+    }
+
+    public int search(String txt) {
+        // Simulate operation of DFA on txt.
+        int i, j, N = txt.length(), M = pat.length();
+        for(i = 0, j= 0; i < N && j < M; i++)
+            j = dfa[txt.charAt(i)][j];
+        if(j == M) return i - M;        //  found
+        return N;                       //  not found
+    }
+
+    public static void main(String[] args) {
+        String pat = args[0];
+        String txt = args[1];
+        KMP kmp = new KMP(pat);
+        int offset = kmp.search(txt);
+        System.out.println("text:    " + txt);
+        System.out.print("pattern: ");
+        for (int i = 0; i < offset; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(pat);
+    }
+}
